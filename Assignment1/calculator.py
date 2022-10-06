b4da0672741f2df74a872cf3845bbc3f1068b0fc
reny1 <reny1@tcd.ie>
2022-10-06 23:28:54 +0100

    Update functions for negative cases

--- Assignment1/calculator.py
+++ Assignment1/calculator.py
@@ -6,21 +6,40 @@ def readInput(inputStr):
     for ch in inputStr:
         if ch not in ["0","1","2","3","4","5","6","7","8","9","+","-","*"," "]:
             print("Error: unexpected character.")
-            exit()
+            return -1
     # set a list containing all operands and operators
     lst = re.findall(r"\d+|[+\-*]", inputStr)
+    # check for missing operands
+    if lst[0] in ["+", "*"] or lst[len(lst)-1] in ["+","-","*"] or (lst[0]=="-" and lst[1] in ["+","-","*"]):
+        print("Error: Missing operands.")
+        return -2
     # check for duplicated operands or operators
     digtNext = True
-    for item in lst:
+    minus = False
+    for i, item in enumerate(lst):
         if item.isdigit() != digtNext:
             if digtNext:
-                print("Error: Duplicated operators.")
+                if item != "-":
+                    print("Error: Duplicated operators.")
+                    return -3
+                else:
+                    minus = True
+                    lst[i] = "n"
             else:
                 print("Error: Duplicated operands.")
-            exit()
+                return -4
         else:
+            if digtNext and minus:
+                lst[i] = "-" + item
+                minus = False
             digtNext = not digtNext
     # return the list if the expression is valid
+    nclear = False
+    while not nclear:
+        if "n" not in lst:
+            nclear = True
+        else:
+            lst.remove("n")
     return lst
 
 def calculate(num1, operator, num2):
@@ -38,7 +57,7 @@ def infixToPostfix(infixlst):
     postfixlst = []
     precedence = {"+":1, "-":1, "*":2}
     for item in infixlst:
-        if item.isdigit():
+        if item.isdigit() or item.lstrip("-").isdigit():
             postfixlst.append(item)
         else:
             if not stack.empty() and precedence[item] <= stacktop:
@@ -53,13 +72,14 @@ def infixToPostfix(infixlst):
         postfixlst.append(stack.get())
     return postfixlst
 
-def runCalculator(inputString):
-    lst = readInput(inputString)
+def runCalculator(lst):
     postfixlst = infixToPostfix(lst)
     stack = LifoQueue()
     solution = 0
+    if len(lst) == 1:
+        return lst[0]
     for item in postfixlst:
-        if item.isdigit():
+        if item.isdigit() or item.lstrip("-").isdigit():
             stack.put(item)
         else:
             num2 = stack.get()
--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,6 +1,11 @@
 import calculator
 
 if __name__ == '__main__':
-    inputString = input("Please enter a mathematical expression: ")
-    sol = calculator.runCalculator(inputString)
+    read = True
+    while read:
+        inputString = input("Please enter a mathematical expression: ")
+        lst = calculator.readInput(inputString)
+        if lst not in [-1, -2, -3, -4]:
+            read = False
+    sol = calculator.runCalculator(lst)
     print(sol)
