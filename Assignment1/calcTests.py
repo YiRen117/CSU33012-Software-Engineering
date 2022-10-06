21c014a305dfe2d03a0936092db41cb689fbb85c
jawood25 <99403690+jawood25@users.noreply.github.com>
2022-10-06 18:13:10 +0100

    fixed errors in first test

--- Assignment1/calcTests.py
+++ Assignment1/calcTests.py
@@ -5,16 +5,16 @@ from calculator import *
 
 
 def test_run_calculator():
-    assert runCalculator("123+456") == 579
-    assert runCalculator("1+2+3+4+5") == 15
-    assert runCalculator("15-12") == 3
-    assert runCalculator("100-50-10-15") == 25
-    assert runCalculator("2x3") == 6
-    assert runCalculator("14x15x7") == 1470
-    assert runCalculator("6x3-7") == 11
-    assert runCalculator("12+14-5+8+2") == 31
-    assert runCalculator("100x3-200+100-50") == 150
-    assert runCalculator("200-12x6") == 128
+    assert runCalculator("123+456") == '579'
+    assert runCalculator("1+2+3+4+5") == '15'
+    assert runCalculator("15-12") == '3'
+    assert runCalculator("100-50-10-15") == '25'
+    assert runCalculator("2*3") == '6'
+    assert runCalculator("14*15*7") == '1470'
+    assert runCalculator("6*3-7") == '11'
+    assert runCalculator("12+14-5+8+2") == '31'
+    assert runCalculator("100*3-200+100-50") == '150'
+    assert runCalculator("200-12*6") == '128'
 
 
 @patch('sys.stdout', new_callable=io.StringIO)
