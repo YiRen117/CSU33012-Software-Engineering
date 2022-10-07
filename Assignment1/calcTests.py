41ce08c9c4fe4be77a89ee25c8830cc957df112f
jawood25 <99403690+jawood25@users.noreply.github.com>
2022-10-07 11:39:48 +0100

    added new tests, amended outdated tests

--- Assignment1/calcTests.py
+++ Assignment1/calcTests.py
@@ -1,27 +1,35 @@
-from unittest.mock import patch
-import io
-
 from calculator import *
 
 
 def test_run_calculator():
-    assert runCalculator("123+456") == '579'
-    assert runCalculator("1+2+3+4+5") == '15'
-    assert runCalculator("15-12") == '3'
-    assert runCalculator("100-50-10-15") == '25'
-    assert runCalculator("2*3") == '6'
-    assert runCalculator("14*15*7") == '1470'
-    assert runCalculator("6*3-7") == '11'
-    assert runCalculator("12+14-5+8+2") == '31'
-    assert runCalculator("100*3-200+100-50") == '150'
-    assert runCalculator("200-12*6") == '128'
+    assert runCalculator(['123', '+', '456']) == '579'
+    assert runCalculator(['1', '+', '2', '+', '3', '+', '4', '+', '5']) == '15'
+    assert runCalculator(['15', '-', '12']) == '3'
+    assert runCalculator(['100', '-', '50', '-', '10', '-', '15']) == '25'
+    assert runCalculator(['2', '*', '3']) == '6'
+    assert runCalculator(['14', '*', '15', '*', '7']) == '1470'
+    assert runCalculator(['6', '*', '3', '-', '7']) == '11'
+    assert runCalculator(['12', '+', '14', '-', '5', '+', '8', '+', '2']) == '31'
+    assert runCalculator(['100', '*', '3', '-', '200', '+', '100', '-', '50']) == '150'
+    assert runCalculator(['200', '-', '12', '*', '6']) == '128'
+    assert runCalculator(['-2', '*', '6']) == '-12'
+    assert runCalculator(['12', '+', '-4']) == '8'
+    assert runCalculator(['18', '-', '-6']) == '24'
 
 
-@patch('sys.stdout', new_callable=io.StringIO)
-def test_read_input_invalid_inputs(mock_stdout):
-    readInput("200++15")
-    assert mock_stdout.getvalue() == "Error: Duplicated operators.\n"
-    readInput("invalid input")
-    assert mock_stdout.getvalue() == "Error: unexpected character.\n"
-    readInput("200 100+10")
-    assert mock_stdout.getvalue() == "Error: Duplicated operands.\n"
+def test_read_input_invalid_inputs():
+    assert readInput("invalid input") == -1
+    assert readInput("+-*") == -2
+    assert readInput("20++10") == -3
+    assert readInput("20 15") == -4
+
+
+def test_infix_to_postfix():
+    assert infixToPostfix(['5', '+', '6', '-', '7']) == ['5', '6', '+', '7', '-']
+    assert infixToPostfix(['3', '+', '2', '*', '4']) == ['3', '2', '4', '*', '+']
+
+
+def test_calculate():
+    assert calculate('5', '+', '5') == '10'
+    assert calculate('5', '-', '3') == '2'
+    assert calculate('5', '*', '5') == '25'
